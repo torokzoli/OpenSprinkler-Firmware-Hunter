2836c2b5f419599cb440ad6708bb64815bd98f65
torokzoli <8179503+torokzoli@users.noreply.github.com>
2025-04-07 11:36:39 +0200

    formattig

--- hunter.cpp
+++ hunter.cpp
@@ -9,22 +9,22 @@
 // val - value to write
 // len - len in bits of the value
 /////////////////////////////////////////////////////////////////////////////
-void HunterBitfield(vector <uint8_t>& bits, uint8_t pos, uint8_t val, uint8_t len)
+void HunterBitfield(vector<uint8_t> &bits, uint8_t pos, uint8_t val, uint8_t len)
 {
-while (len > 0)
-{
-if (val & 0x1)
-{
-bits[pos / 8] = bits[pos / 8] | 0x80 >> (pos % 8);
-}
-else
-{
-bits[pos / 8] = bits[pos / 8] & ~(0x80 >> (pos % 8));
-}
-len--;
-val = val >> 1;
-pos++;
-}
+  while (len > 0)
+  {
+    if (val & 0x1)
+    {
+      bits[pos / 8] = bits[pos / 8] | 0x80 >> (pos % 8);
+    }
+    else
+    {
+      bits[pos / 8] = bits[pos / 8] & ~(0x80 >> (pos % 8));
+    }
+    len--;
+    val = val >> 1;
+    pos++;
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -34,10 +34,10 @@ pos++;
 /////////////////////////////////////////////////////////////////////////////
 void HunterLow(void)
 {
-digitalWrite(HUNTER_PIN, HUNTER_ONE);
-delayMicroseconds(SHORT_INTERVAL); //microseconds
-digitalWrite(HUNTER_PIN, HUNTER_ZERO);
-delayMicroseconds(LONG_INTERVAL); //microseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ONE);
+  delayMicroseconds(SHORT_INTERVAL); // microseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ZERO);
+  delayMicroseconds(LONG_INTERVAL); // microseconds
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -47,10 +47,10 @@ delayMicroseconds(LONG_INTERVAL); //microseconds
 /////////////////////////////////////////////////////////////////////////////
 void HunterHigh(void)
 {
-digitalWrite(HUNTER_PIN, HUNTER_ONE);
-delayMicroseconds(LONG_INTERVAL); //microseconds
-digitalWrite(HUNTER_PIN, HUNTER_ZERO);
-delayMicroseconds(SHORT_INTERVAL); //microseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ONE);
+  delayMicroseconds(LONG_INTERVAL); // microseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ZERO);
+  delayMicroseconds(SHORT_INTERVAL); // microseconds
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -61,59 +61,58 @@ delayMicroseconds(SHORT_INTERVAL); //microseconds
 /////////////////////////////////////////////////////////////////////////////
 void HunterWrite(vector<uint8_t> buffer, bool extrabit)
 {
-// Bus start sequence
-//_flexwire.invert(false);
-//delay(325);
-//_flexwire.invert(true);
-//delay(65);
+  // Bus start sequence
+  //_flexwire.invert(false);
+  // delay(325);
+  //_flexwire.invert(true);
+  // delay(65);
 
-// Resetimpulse
-digitalWrite(HUNTER_PIN, HUNTER_ONE);
-delay(325); //milliseconds
-digitalWrite(HUNTER_PIN, HUNTER_ZERO);
-delay(65); //milliseconds
+  // Resetimpulse
+  digitalWrite(HUNTER_PIN, HUNTER_ONE);
+  delay(325); // milliseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ZERO);
+  delay(65); // milliseconds
 
+  // Write the start pulse
+  //_flexwire.invert(false);
+  // udelay(900);
+  //_flexwire.invert(true);
 
-// Write the start pulse
-//_flexwire.invert(false);
-//udelay(900);
-//_flexwire.invert(true);
+  // Startimpulse
+  digitalWrite(HUNTER_PIN, HUNTER_ONE);
+  delayMicroseconds(START_INTERVAL); // microseconds
+  digitalWrite(HUNTER_PIN, HUNTER_ZERO);
+  delayMicroseconds(SHORT_INTERVAL); // microseconds
 
-// Startimpulse
-digitalWrite(HUNTER_PIN, HUNTER_ONE);
-delayMicroseconds(START_INTERVAL); //microseconds
-digitalWrite(HUNTER_PIN, HUNTER_ZERO);
-delayMicroseconds(SHORT_INTERVAL); //microseconds
-
-// Write the bits out
-//_flexwire.write(buffer);
-for (auto &sendByte : buffer)
-{
-  for (uint8_t inner = 0; inner < 8; inner++)
+  // Write the bits out
+  //_flexwire.write(buffer);
+  for (auto &sendByte : buffer)
   {
-    // Send high order bits first
-    if (sendByte & 0x80)
+    for (uint8_t inner = 0; inner < 8; inner++)
     {
-      HunterHigh();
+      // Send high order bits first
+      if (sendByte & 0x80)
+      {
+        HunterHigh();
+      }
+      else
+      {
+        HunterLow();
+      }
+      sendByte <<= 1;
     }
-    else
-    {
-      HunterLow();
-    }
-    sendByte <<= 1;
   }
-}
 
-// Include an extra 1 bit
-if (extrabit)
-{
-  //_flexwire.writebit(true);
-  HunterHigh();
-}
+  // Include an extra 1 bit
+  if (extrabit)
+  {
+    //_flexwire.writebit(true);
+    HunterHigh();
+  }
 
-// Write the stop pulse
-//_flexwire.writebit(false);
-HunterLow();
+  // Write the stop pulse
+  //_flexwire.writebit(false);
+  HunterLow();
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -124,66 +123,66 @@ HunterLow();
 /////////////////////////////////////////////////////////////////////////////
 void HunterStart(uint8_t zone, uint8_t time)
 {
-DEBUG_PRINT("X-Core Hunter start zone ");
-DEBUG_PRINT(zone);
-DEBUG_PRINT(" for ");
-DEBUG_PRINT(time);
-DEBUG_PRINTLN(" min");
+  DEBUG_PRINT("X-Core Hunter start zone ");
+  DEBUG_PRINT(zone);
+  DEBUG_PRINT(" for ");
+  DEBUG_PRINT(time);
+  DEBUG_PRINTLN(" min");
 
-// Start out with a base frame 
-vector<uint8_t>  buffer = {0xff,0x00,0x00,0x00,0x10,0x00,0x00,0x04,0x00,0x00,0x01,0x00,0x01,0xb8,0x3f};
+  // Start out with a base frame
+  vector<uint8_t> buffer = {0xff, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x04, 0x00, 0x00, 0x01, 0x00, 0x01, 0xb8, 0x3f};
 
-if (zone < 1 || zone > 48)
-{
+  if (zone < 1 || zone > 48)
+  {
     DEBUG_PRINTLN("invalid zone");
     return;
-}
+  }
 
-if (time < 0 || time > 240)
-{
+  if (time < 0 || time > 240)
+  {
     DEBUG_PRINTLN("invalid time");
     return;
-}
+  }
 
-// The bus protocol is a little bizzare, not sure why
+  // The bus protocol is a little bizzare, not sure why
 
-// Bits 9:10 are 0x1 for zones > 12 and 0x2 otherwise
-if (zone > 12)
-{
+  // Bits 9:10 are 0x1 for zones > 12 and 0x2 otherwise
+  if (zone > 12)
+  {
     HunterBitfield(buffer, 9, 0x1, 2);
-}
-else
-{
+  }
+  else
+  {
     HunterBitfield(buffer, 9, 0x2, 2);
-}
+  }
 
-// Zone + 0x17 is at bits 23:29 and 36:42
-HunterBitfield(buffer, 23, zone + 0x17, 7);
-HunterBitfield(buffer, 36, zone + 0x17, 7);
+  // Zone + 0x17 is at bits 23:29 and 36:42
+  HunterBitfield(buffer, 23, zone + 0x17, 7);
+  HunterBitfield(buffer, 36, zone + 0x17, 7);
 
-// Zone + 0x23 is at bits 49:55 and 62:68
-HunterBitfield(buffer, 49, zone + 0x23, 7);
-HunterBitfield(buffer, 62, zone + 0x23, 7);
+  // Zone + 0x23 is at bits 49:55 and 62:68
+  HunterBitfield(buffer, 49, zone + 0x23, 7);
+  HunterBitfield(buffer, 62, zone + 0x23, 7);
 
-// Zone + 0x2f is at bits 75:81 and 88:94
-HunterBitfield(buffer, 75, zone + 0x2f, 7);
-HunterBitfield(buffer, 88, zone + 0x2f, 7);
+  // Zone + 0x2f is at bits 75:81 and 88:94
+  HunterBitfield(buffer, 75, zone + 0x2f, 7);
+  HunterBitfield(buffer, 88, zone + 0x2f, 7);
 
-// Time is encoded in three places and broken up by nibble
-// Low nibble:  bits 31:34, 57:60, and 83:86
-// High nibble: bits 44:47, 70:73, and 96:99
-HunterBitfield(buffer, 31, time, 4);
-HunterBitfield(buffer, 44, time >> 4, 4);
-HunterBitfield(buffer, 57, time, 4);
-HunterBitfield(buffer, 70, time >> 4, 4);
-HunterBitfield(buffer, 83, time, 4);
-HunterBitfield(buffer, 96, time >> 4, 4);
+  // Time is encoded in three places and broken up by nibble
+  // Low nibble:  bits 31:34, 57:60, and 83:86
+  // High nibble: bits 44:47, 70:73, and 96:99
+  HunterBitfield(buffer, 31, time, 4);
+  HunterBitfield(buffer, 44, time >> 4, 4);
+  HunterBitfield(buffer, 57, time, 4);
+  HunterBitfield(buffer, 70, time >> 4, 4);
+  HunterBitfield(buffer, 83, time, 4);
+  HunterBitfield(buffer, 96, time >> 4, 4);
 
-// Bottom nibble of zone - 1 is at bits 109:112
-HunterBitfield(buffer, 109, zone - 1, 4);
+  // Bottom nibble of zone - 1 is at bits 109:112
+  HunterBitfield(buffer, 109, zone - 1, 4);
 
-// Write the bits out of the bus
-HunterWrite(buffer, true);
+  // Write the bits out of the bus
+  HunterWrite(buffer, true);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -193,10 +192,10 @@ HunterWrite(buffer, true);
 /////////////////////////////////////////////////////////////////////////////
 void HunterStop(uint8_t zone)
 {
-DEBUG_PRINT("X-Core Hunter stop zone ");
-DEBUG_PRINT(zone);
-DEBUG_PRINT(" = ");
-HunterStart(zone, 0);
+  DEBUG_PRINT("X-Core Hunter stop zone ");
+  DEBUG_PRINT(zone);
+  DEBUG_PRINT(" = ");
+  HunterStart(zone, 0);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -206,16 +205,16 @@ HunterStart(zone, 0);
 /////////////////////////////////////////////////////////////////////////////
 void HunterProgram(uint8_t num)
 {
-// Start with a basic program frame
-vector<uint8_t> buffer = {0xff, 0x40, 0x03, 0x96, 0x09 ,0xbd ,0x7f};
+  // Start with a basic program frame
+  vector<uint8_t> buffer = {0xff, 0x40, 0x03, 0x96, 0x09, 0xbd, 0x7f};
 
-if (num < 1 || num > 4)
-{
+  if (num < 1 || num > 4)
+  {
     DEBUG_PRINTLN("invalid program");
     return;
-}
+  }
 
-// Program number - 1 is at bits 31:32
-HunterBitfield(buffer, 31, num - 1, 2);
-HunterWrite(buffer, false);
+  // Program number - 1 is at bits 31:32
+  HunterBitfield(buffer, 31, num - 1, 2);
+  HunterWrite(buffer, false);
 }
